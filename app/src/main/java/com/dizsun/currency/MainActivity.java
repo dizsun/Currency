bf03632f336043680fa526ba7b38eab09c357577
dizsun <dizsun@icloud.com>
2016-11-24 21:13:36 +0800

    实现共享偏好设置

--- app/src/main/java/com/dizsun/currency/MainActivity.java
+++ app/src/main/java/com/dizsun/currency/MainActivity.java
@@ -28,7 +28,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private EditText mAmountEditText;
     private Spinner mForSpinner, mHomSpinner;
 
-    private String[] mCurrenices;
+    private String[] mCurrencies;
+    public static final String FOR = "FOR_CURRENCY";
+    public static final String HOM = "HOM_CURRENCY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +40,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         setSupportActionBar(toolbar);
 
         //将货币ArrayList解压并转化为数组
-        ArrayList<String> arrayList = (ArrayList<String>) getIntent().getSerializableExtra(SplashActivity.NAME);
+        ArrayList<String> arrayList =
+                (ArrayList<String>) getIntent().getSerializableExtra(SplashActivity.NAME);
         Collections.sort(arrayList);
-        mCurrenices = arrayList.toArray(new String[arrayList.size()]);
+        mCurrencies = arrayList.toArray(new String[arrayList.size()]);
 
         //空间初始化
         mConvertedTextView = (TextView) findViewById(R.id.txt_converted);
@@ -50,13 +53,26 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         mHomSpinner = (Spinner) findViewById(R.id.spn_hom);
 
         //List的适配器
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.spinner_closed, mCurrenices);
+        ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<String>(this, R.layout.spinner_closed, mCurrencies);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mHomSpinner.setAdapter(arrayAdapter);
         mForSpinner.setAdapter(arrayAdapter);
         //将监听器加到MainActivity上
         mHomSpinner.setOnItemSelectedListener(this);
         mForSpinner.setOnItemSelectedListener(this);
+
+        //设置默认偏好
+        if(savedInstanceState==null&&
+                (PrefsMgr.getString(this,FOR)==null&&PrefsMgr.getString(this,HOM)==null)){
+            mForSpinner.setSelection(findPositionGivenCode("USD",mCurrencies));
+            mHomSpinner.setSelection(findPositionGivenCode("CNY",mCurrencies));
+            PrefsMgr.setString(this,FOR,"USD");
+            PrefsMgr.setString(this,HOM,"CNY");
+        }else {
+            mForSpinner.setSelection(findPositionGivenCode(PrefsMgr.getString(this,FOR),mCurrencies));
+            mForSpinner.setSelection(findPositionGivenCode(PrefsMgr.getString(this,HOM),mCurrencies));
+        }
     }
 
     public boolean isOnline() {
@@ -80,6 +96,34 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         mForSpinner.setSelection(nHom);
         mHomSpinner.setSelection(nFor);
         mConvertedTextView.setText("");
+        PrefsMgr.setString(this,FOR,extractCodeFromCurrency((String)mForSpinner.getSelectedItem()));
+        PrefsMgr.setString(this,HOM,extractCodeFromCurrency((String)mForSpinner.getSelectedItem()));
+    }
+
+    /**
+     * 根据给的货币代码查找在spinner上的位置
+     *
+     * @param code       给定的货币代码
+     * @param currencies 货币聚合字符串数组
+     * @return 在spinner上的位置，默认为第一位
+     */
+    private int findPositionGivenCode(String code, String[] currencies) {
+        for (int i = 0; i < currencies.length; i++) {
+            if (extractCodeFromCurrency(currencies[i]).equalsIgnoreCase(code)) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * 从聚合字符串中抽取货币代码
+     *
+     * @param currency 聚合字符串
+     * @return 货币代码
+     */
+    private String extractCodeFromCurrency(String currency) {
+        return (currency).substring(0, 3);
     }
 
     @Override
@@ -89,6 +133,12 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         return true;
     }
 
+    /**
+     * 当spinner的选项被选中时
+     *
+     * @param item 被选中的选项
+     * @return
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
@@ -110,14 +160,17 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        switch (adapterView.getId()){
+        switch (adapterView.getId()) {
             case R.id.spn_for:
+                PrefsMgr.setString(this,FOR,extractCodeFromCurrency((String)mForSpinner.getSelectedItem()));
                 break;
             case R.id.spn_hom:
+                PrefsMgr.setString(this,HOM,extractCodeFromCurrency((String)mForSpinner.getSelectedItem()));
                 break;
             default:
                 break;
         }
+        mConvertedTextView.setText("");
     }
 
     @Override
